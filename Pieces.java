61f1cbdd4b1b24fb491ae6c620a584ae2e6f149e
BorschtStroganoff <144555524+BorschtStroganoff@users.noreply.github.com>
2023-09-09 18:17:54 -0400

    Create Pieces.java

---
+++ Pieces.java
@@ -0,0 +1,16 @@
+/**
+* This class represents each square on the chess board.
+* It has a piece and a color (or empty)
+*/
+public class Pieces {
+
+   /** stored values */
+   int piece;
+   int color;
+
+   public Pieces(int piece, int color) {
+       this.piece = piece;
+       this.color = color;
+   }
+
+} 
